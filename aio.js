18e573c3d2a932d7890c7c3a5f3225672886481f
MEAIE      (Medical Expert Assistant Integrated Enterprise) <playerauth7@gmail.com>
2026-03-14 10:35:43 +0530

    Update aio.js

--- aio.js
+++ aio.js
@@ -1,6 +1,6 @@
 (function() {
 const loader = document.createElement('div');
-loader.style = "position:fixed;top:0;left:0;width:100vw;height:100vh;background:white;display:flex;align-items:center;justify-content:center;z-index:9999;font-size:20px;font-weight:900";
+loader.style = "position:fixed;top:0;left:0;width:100vw;height:100vh;background:white;display:none;align-items:center;justify-content:center;z-index:9999;font-size:20px;font-weight:900";
 loader.innerHTML = "Loading...";
 document.body.appendChild(loader);
   
